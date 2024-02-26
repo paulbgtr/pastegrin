a05f6c9df0e9df96fa98942b69d7449a6161e485
paulbg <paulbg@hey.com>
2024-02-26 10:46:51 +0200

    feat: implement signup handler
    This handler checks whether a user is already registered and if not, it
    registers the user. It also hashes the password before storing it in the
    database.

--- backend/auth/src/auth.rs
+++ backend/auth/src/auth.rs
@@ -1,24 +1,51 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use crate::db;
 use serde::Deserialize;
+use std::collections::HashMap;
+use bcrypt::{DEFAULT_COST, hash, verify};
 
 #[derive(Deserialize)]
-struct SigninRequest {
+struct UserRequest {
     email: String,
     password: String,
 }
 
-#[post("/signin")]
-async fn signin(info: web::Json<SigninRequest>) -> impl Responder {
+#[post("/signup")]
+async fn signup(info: web::Json<UserRequest>) -> impl Responder {
     let (email, password) = (&info.email, &info.password);
 
     let conn = &mut db::establish_connection();
 
     let user = db::get_user_by_email(conn, email);
 
+    if user.is_some() {
+        return HttpResponse::Conflict().body("User already exists");
+    }
+
+    let hashed_password = hash(password, DEFAULT_COST).expect("Failed to hash password");
+
+    let user = db::create_user(conn, email, &hashed_password);
+
+    HttpResponse::Ok().body("User created")
+}
+
+#[post("/signin")]
+async fn signin(info: web::Json<UserRequest>) -> impl Responder {
+    let (email, password) = (&info.email, &info.password);
+
+    let conn = &mut db::establish_connection();
+
+    let user = db::get_user_by_email(conn, email);
+
+    if user.is_none() {
+        return HttpResponse::NotFound().body("User not found");
+    }
+
     HttpResponse::Ok().finish()
 }
 
 pub fn auth_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(signin);
+    cfg
+    .service(signin)
+    .service(signup);
 }
