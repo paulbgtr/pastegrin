3ccb46164e469b31657a4ee8e12ad4048344e4b8
paulbg <paulbg@hey.com>
2024-02-27 11:19:27 +0200

    feat: helpers mod with generate_jwt util

---
+++ backend/auth/src/helpers.rs
@@ -0,0 +1,21 @@
+use jwt_simple::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct AdditionalData {
+    user_id: i32,
+}
+
+const KEY: &str = "secret";
+
+pub fn generate_jwt(user_id: i32) -> String {
+    let key = HS256Key::from_bytes(KEY.as_bytes());
+
+    let additional_data = AdditionalData { user_id };
+
+    let claims = Claims::with_custom_claims(additional_data, Duration::from_days(30));
+
+    let token = key.authenticate(claims).unwrap();
+
+    token
+}
