429cd4ebbedefd8bfd9b66ecc6934010beef4903
paulbg <paulbg@hey.com>
2024-03-14 11:54:57 +0200

    feat: add typeorm config to app module

--- backend/pastes/src/modules/app.module.ts
+++ backend/pastes/src/modules/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Paste } from 'src/model/paste.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'pastes',
+      entities: [Paste],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
