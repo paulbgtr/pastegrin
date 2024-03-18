296d8e57ac0e2448047538420eb5a13f2ced87e3
paulbg <paulbg@hey.com>
2024-03-18 16:04:33 +0200

    feat: import pastes module to app module

--- backend/pastes/src/modules/app.module.ts
+++ backend/pastes/src/modules/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Paste } from 'src/model/paste.entity';
+import { PastesModule } from './pastes.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
+      autoLoadEntities: true,
       type: 'postgres',
       host: 'localhost',
       port: 5433,
@@ -16,6 +18,7 @@ import { Paste } from 'src/model/paste.entity';
       entities: [Paste],
       synchronize: true,
     }),
+    PastesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
