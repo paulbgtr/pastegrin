4bb1baa74433f8f7747eceb6e0e14003ce92cb3b
paulbg <paulbg@hey.com>
2024-03-21 10:38:49 +0200

    feat: AddPaste component with a functional form

--- frontend/src/rootsComponents/root/AddPaste.tsx
+++ frontend/src/rootsComponents/root/AddPaste.tsx
@@ -1,18 +1,85 @@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  content: z.string().min(1, {
+    message: "Content cannot be empty",
+  }),
+  title: z.string(),
+  password: z.string(),
+});
 
 export const AddPaste = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: "",
+      title: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-      {/* todo: implement form */}
-      <h2 className="mb-2">New Paste</h2>
-      <div className="grid gap-3">
-        <Textarea placeholder="Content" className="min-h-[240px]" />
-        <Input type="text" placeholder="Title" />
-        <Input type="password" placeholder="Password" />
-      </div>
-      <Button className="mt-3">Create new paste</Button>
-    </div>
+    <>
+      <h2 className="mb-1">New Paste</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    placeholder="Content"
+                    className="min-h-[240px]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="text" placeholder="Title" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="password" placeholder="Password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
   );
 };
