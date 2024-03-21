859da12c8122d82bd1813c60660e7e4e05c5dd32
paulbg <paulbg@hey.com>
2024-03-21 16:33:45 +0200

    feat: populating data on the pastes dynamic route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Root from "./roots/root";
-import ErrorPage from "./roots/error-page";
+import Paste from "./roots/paste";
+import PageNotFound from "./roots/errors/pageNotFound";
+import PasteNotFound from "./roots/errors/pasteNotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />,
+    errorElement: <PageNotFound />,
+  },
+  {
+    path: "/pastes/:pasteId",
+    loader: async ({ request, params }) => {
+      return fetch(`http://localhost:3000/pastes/pastes/${params.pasteId}`, {
+        signal: request.signal,
+      });
+    },
+    element: <Paste />,
+    errorElement: <PasteNotFound />,
   },
 ]);
 
