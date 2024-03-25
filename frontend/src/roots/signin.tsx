2a7aa6f30d936ca2376492bacc8fdaa19a8d96ca
paulbg <paulbg@hey.com>
2024-03-25 18:21:19 +0200

    feat: implement signin route

---
+++ frontend/src/roots/signin.tsx
@@ -0,0 +1,13 @@
+import { SignInForm } from "@/rootsComponents/signin/SignInForm";
+
+const SignIn = () => {
+  return (
+    <div className="h-screen">
+      <div className="grid items-center justify-center w-full h-full">
+        <SignInForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
