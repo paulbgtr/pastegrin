eff0698d97b09f6bf3dccb5ec8be979d5794f889
paulbg <paulbg@hey.com>
2024-03-25 18:20:34 +0200

    feat: implement SignInForm component
    This is a basic form component that accepts email and password as input
    and sends them to the server for authentication.
    If the authentication is successful, the user views a success message.

---
+++ frontend/src/rootsComponents/signin/SignInForm.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+const formSchema = z.object({
+  email: z.string().min(1, {
+    message: "Content cannot be empty",
+  }),
+  password: z.string(),
+});
+
+export const SignInForm = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const apiUrl = "http://localhost:3000/auth/signin";
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (response.ok) setIsSignedIn(true);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  if (isSignedIn) {
+    return (
+      <div>
+        <h2 className="mb-2 text-2xl font-bold text-center">Sign In</h2>
+        <p className="mb-3 text-center">You are now signed in!</p>
+        <Button
+          className="w-full"
+          onClick={() => {
+            navigate("/pastes/new");
+          }}
+        >
+          Create a new paste
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2 className="mb-2 text-2xl font-bold text-center">Sign In</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="text" placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="password" placeholder="Password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
