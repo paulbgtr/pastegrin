a979be9326c9578a4074a3c67948c70f6178c68f
paulbg <paulbg@hey.com>
2024-03-25 17:59:52 +0200

    feat: add userId column to paste entity

--- backend/pastes/src/model/paste.entity.ts
+++ backend/pastes/src/model/paste.entity.ts
@@ -5,12 +5,18 @@ export class Paste {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({
+    name: 'user_id',
+    nullable: true,
+  })
+  userId?: number;
+
   @Column()
   title: string;
 
   @Column()
   content: string;
 
-  @Column()
-  password: string;
+  @Column({ nullable: true })
+  password?: string;
 }
