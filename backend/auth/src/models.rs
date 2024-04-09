2d77bb64b97b41e89f4c715aea8c933b6868fa0b
paulbg <paulbg@hey.com>
2024-04-09 08:42:12 +0300

    feat: make User model derive Serialize and Deserialize

--- backend/auth/src/models.rs
+++ backend/auth/src/models.rs
@@ -1,9 +1,11 @@
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 use std::time::SystemTime;
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub email: String,
