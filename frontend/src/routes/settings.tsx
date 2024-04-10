637c4bbb59f1dbf9a8abfd645b9aa5eecf0b0893
paulbg <paulbg@hey.com>
2024-04-10 13:55:45 +0300

    feat: settings page implementation
    This page fetches the user data and then passes it to the
    UpdateAccount component.

--- frontend/src/routes/settings.tsx
+++ frontend/src/routes/settings.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UpdateAccount } from "../routesComponents/settings/UpdateAccount";
+import { User } from "@/types/User";
+
 const Settings = () => {
+  const [user, setUser] = useState<User>(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) navigate("/signin");
+
+      const response = await fetch("http://localhost:3000/auth/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+
+      if (!response.ok) {
+        navigate("/signin");
+        return;
+      }
+
+      setUser(data);
+    };
+
+    fetchUser();
+  }, []);
+
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className="grid max-w-5xl mx-auto">
+      <h1 className="text-2xl font-bold">Settings</h1>
+      {user && <UpdateAccount user={user} />}
     </div>
   );
 };
