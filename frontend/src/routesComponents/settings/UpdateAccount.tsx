3ce360ba8c53c6cd7293db2f4e338a4ecb6a6331
paulbg <paulbg@hey.com>
2024-04-10 14:33:22 +0300

    feat: integrate update account form with backend

--- frontend/src/routesComponents/settings/UpdateAccount.tsx
+++ frontend/src/routesComponents/settings/UpdateAccount.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -13,20 +14,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { User } from "@/types/User";
 
-const formSchema = z
-  .object({
-    email: z.string().min(1, {
-      message: "Email cannot be empty",
-    }),
-    password: z.string(),
-    oldPassword: z.string(),
-  })
-  .refine((data) => data.password && data.oldPassword, {
-    message: "Old password is required",
-    path: ["oldPassword"],
-  });
+const formSchema = z.object({
+  email: z.string().optional(),
+  password: z.string().optional(),
+  oldPassword: z.string().optional(),
+});
 
 export const UpdateAccount = ({ user }: { user: User }) => {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -36,7 +33,35 @@ export const UpdateAccount = ({ user }: { user: User }) => {
     },
   });
 
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {};
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const token = localStorage.getItem("token");
+
+    if (!token) return;
+
+    const requestBody = {
+      ...(values.email && { email: values.email }),
+      ...(values.password && { new_password: values.password }),
+      ...(values.oldPassword && { old_password: values.oldPassword }),
+    };
+
+    const res = await fetch(`http://localhost:3000/auth/users/${user.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (res.ok) {
+      setError(null);
+
+      setIsSuccess(true);
+      return;
+    }
+
+    const err = await res.text();
+    setError(err);
+  };
 
   return (
     <div>
@@ -88,6 +113,16 @@ export const UpdateAccount = ({ user }: { user: User }) => {
               />
             )}
             <Button type="submit">Update Account</Button>
+            {error && (
+              <div className="p-3 text-center text-red-500 rounded-md">
+                {error}
+              </div>
+            )}
+            {isSuccess && (
+              <div className="p-3 text-center text-green-500 rounded-md">
+                Account updated successfully
+              </div>
+            )}
           </div>
         </form>
       </Form>
